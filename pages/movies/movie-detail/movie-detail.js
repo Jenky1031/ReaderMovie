495803353f83ce8f26e64afcb0184efbc6db368a
Jenky1031 <869386961@qq.com>
2020-03-15 00:42:59 +0800

    class 增加Movie类 改写movie-detail

--- pages/movies/movie-detail/movie-detail.js
+++ pages/movies/movie-detail/movie-detail.js
@@ -1,5 +1,9 @@
+import {
+    Movie
+} from 'class/Movie.js'
+
 let app = getApp()
-let util = require('../../../utils/util.js')
+
 Page({
 
     /**
@@ -15,49 +19,16 @@ Page({
     onLoad: function(options) {
         let movieId = options.id
         let baseUrl = app.globalData.doubanBase
-        let http = util.http
         let url = baseUrl + '/v2/movie/subject/' + movieId
-        http(url, this.processDoubanData)
-    },
 
-    processDoubanData: function(data) {
-        if (!data) {
-            return
-        }
-        let director = {
-            avatar: '',
-            name: '',
-            id: ''
-        }
-        // 判空 有导演
-        if (data.directors[0] != null) {
-            if (data.directors[0].avatars != null) {
-                // 有导演 有图片
-                director.avatar = data.directors[0].avatars.large
-            }
-            director.name = data.directors[0].name
-            director.id = data.directors[0].id
-        }
-        let movie = {
-            movieImg: data.images ? data.images.large : '',
-            country: data.countries[0],
-            title: data.title,
-            originalTitle: data.original_title,
-            wishCount: data.wish_count,
-            commentCount: data.comments_count,
-            year: data.year,
-            genres: data.genres.join('、'),
-            stars: util.converToStarsArray(data.rating.stars),
-            score: data.rating.average,
-            director: director,
-            casts: util.convertToCastString(data.casts),
-            castsInfo: util.convertToCastInfos(data.casts),
-            summary: data.summary
-        }
-        this.setData({
-            movie: movie
+        let movie = new Movie(url)
+        movie.getMovieData(movie => {
+            this.setData({
+                movie: movie
+            })
         })
     },
+
     // 查看图片
     viewMoviePostImg: function(event) {
         let src = event.currentTarget.dataset.src;
